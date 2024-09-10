30ec66d8321e7e5ad3cbbb38f2d703c2c07c06cc
norbert1717 <miklosnorbert17@gmail.com>
2024-09-10 18:21:40 +0200

    created backend, serving static and data

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+
+/* 
+cd mappaváltás
+cd.. előző mappába ugrás */
+
+
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../frontend/index.html'))
+})
+
+app.use('/static', express.static(path.join(__dirname, '/../frontend/static')))
+
+app.get('/data', (req, res) => {
+    fs.readFile(`${__dirname}/data/drinks.json`, (err, data) => {
+        if (err) {
+            console.log("error at reading the file", err)
+            res.json("error at reading the file")
+        } else {
+            const jsonData = JSON.parse(data)
+            const result = jsonData.map(obj => obj)
+            res.json(jsonData)
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
